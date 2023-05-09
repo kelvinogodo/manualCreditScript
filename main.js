7ef4f40f569475c5f9faeb546f66e96623350308
kelvinogodo <kelvinchukwuebuka385@gmail.com>
2023-05-09 07:43:44 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+let email = ''
+let amount = 0
+
+document.getElementById('emailInput').addEventListener('change', (e)=>{
+    email = e.target.value
+    return email
+})
+
+document.getElementById('amountInput').addEventListener('change', (e)=>{
+    amount = parseInt(e.target.value)
+    return amount
+})
+
+const sendRequest = async ()=>{
+    const req = await fetch('http://localhost:5000/api/manualCredit',{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/json',
+        },
+        body:JSON.stringify({
+            email,
+            amount
+        })
+    })
+    const res = await req.json()
+    if(res){
+        if(res.status === 'ok'){
+            alert(`you have successfully credited this user with ${res.amount}`)
+        }
+        else{
+            alert(`sorry an error occurred! ${res.error}`)
+        }
+    }
+}
+
+document.getElementById('credit-form').addEventListener('submit',(e)=>{
+    e.preventDefault()
+    if(isNaN(amount)){
+        alert('amount must be a number')
+        return
+    }else{
+        sendRequest()
+    }
+})
